0ff485235830ce0a15390d34c3b9db357f346e4c
Alex Testa <alex.testa@studio.unibo.it>
2026-03-23 14:11:59 +0100

    refactor: refactor currying file for deprecated annotation

--- src/main/scala/it/unibo/pps/u02/Currying.scala
+++ src/main/scala/it/unibo/pps/u02/Currying.scala
@@ -1,7 +1,5 @@
 package it.unibo.pps.u02
 
-object Currying extends App :
-
   // standard function with no currying
   def mult(x: Double, y: Double): Double = x * y
 
@@ -9,19 +7,22 @@ object Currying extends App :
   // curriedMult has actually type: Double => (Double => Double)
   def curriedMult(x: Double)(y: Double): Double = x * y
 
-  // slightly different syntax at the call side..
-  println(mult(10, 2)) // 20
-  println(curriedMult(10)(2)) // 20
+  @main def runCurrying(): Unit = {
 
-  // curriedMult can be partially applied!
-  val twice: Double => Double = curriedMult(2)
+    // slightly different syntax at the call side..
+    println(mult(10, 2)) // 20
+    println(curriedMult(10)(2)) // 20
 
-  println(twice(10)) // 20
+    // curriedMult can be partially applied!
+    val twice: Double => Double = curriedMult(2)
 
-  // => is right-associative, hence it is equal to:
-  //val curr...: Double => (Double => Double) = x => (y => x*y)
-  val curriedMultAsFunction: Double => Double => Double = x => y => x * y
+    println(twice(10)) // 20
 
-  println(curriedMultAsFunction(10)(2)) // 20
-  println(curriedMultAsFunction) // u02.Currying$$$Lambda$7/...
-  println(curriedMultAsFunction(10)) // u02.Currying$$$Lambda$12/...
+    // => is right-associative, hence it is equal to:
+    //val curr...: Double => (Double => Double) = x => (y => x*y)
+    val curriedMultAsFunction: Double => Double => Double = x => y => x * y
+
+    println(curriedMultAsFunction(10)(2)) // 20
+    println(curriedMultAsFunction) // u02.Currying$$$Lambda$7/...
+    println(curriedMultAsFunction(10)) // u02.Currying$$$Lambda$12/...
+  }
